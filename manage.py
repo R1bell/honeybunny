f45fb798143ae37288b2031de7badeeaeaa604d8
Raphael Sanamyan <raphael.sanamyan@akvelon.com>
2020-03-09 14:53:09 +0300

    tjh

--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ from flask_admin import Admin
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 
 from app.admin import create_admin
@@ -18,17 +17,7 @@ app: Flask = create_app()
 CORS(app)
 db: SQLAlchemy = create_db(app)
 jwt: JWTManager = create_jwt(app)
-manager: Manager = Manager(app)
 migrate: Migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
 admin: Admin = create_admin(app)
 
-
-@manager.command
-def run():
-    system('python manage.py db upgrade')
-    app.run(debug=True, host="0.0.0.0", port=5000)
-
-
-if __name__ == '__main__':
-    manager.run()
+system('flask db upgrade')
